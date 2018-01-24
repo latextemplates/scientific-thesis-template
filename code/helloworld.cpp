53befcbb69e5ab70a9d554c24a200e7b2effbb90
Oliver Kopp <kopp.dev@gmail.com>
2018-01-24 09:49:17 +0100

    Demonstration of lstinputlisting
    Signed-off-by: Oliver Kopp <kopp.dev@gmail.com>
    Also-by: Patrick Diehl <diehl@ins.uni-bonn.de>
    Also-by: thesen@ins.uni-bonn.de
    Also-by: jneuen@ins.uni-bonn.de

---
+++ code/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  // print Hello World to the stdout
+  std::cout << "Hello World!" << std::endl;
+  return EXIT_SUCCESS;
+}
